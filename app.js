8dc0519e97076d9f968471e27218f619c62cd4a3
Matías Herrera <matias7122@gmail.com>
2021-07-27 12:46:58 -0300

    app actualizado

--- app.js
+++ app.js
@@ -13,4 +13,16 @@ app.use(bodyParser.json());
 //load routes
 app.use('/api', routes);
 
-module.exports = app;
+// module.exports = app;
+const mongoose = require('mongoose');
+const port = 3000;
+// const app = require('./app');
+
+mongoose.connect('mongodb+srv://matiash:matiash@carsdb.290x4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('La conexion ah la base de datos ah sido establecida')
+
+        app.listen(port, () => {
+            console.log(`Servidor corriendo en puerto ${port}`)
+        })
+    })
